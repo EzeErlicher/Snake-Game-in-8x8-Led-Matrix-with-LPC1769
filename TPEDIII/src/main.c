db2987aaebbe28948ab58fb73ecf04ce83b0679b
badajuan <badajuan2@gmail.com>
2023-11-03 01:12:53 -0300

    Update Juan - Muevo algunos comentarios de lugar - Implemento checkCollisions() - Implemento updateDirection() - Implemento checkCollisions() (Terminado) - Añado la función stopGame()

--- TPEDIII/src/main.c
+++ TPEDIII/src/main.c
@@ -6,7 +6,6 @@
 typedef struct {
     int x, y;
 } Point;
-
 typedef enum {
     ARRIBA, ABAJO, IZQ, DER
 } Direction;
@@ -24,28 +23,15 @@ void configI2C();        // Comunicación con Matriz Led
 void configEUART();      // Envio de estadisticas
 
 
-/*  Chequea si el proximo movimiento newPos de la vibora contra cuatro situaciones:
-    - Fuera de los limites -> GameOver: Sonido + StopTotal + Enviar stats
-    - Choque contra si misma -> GameOver
-    - Choque con la manzana -> moveSnake + WIN: Sonido + newApple + updateLength
-    - Espacio libre -> moveSnake
-    ---
-       Return: -1 = Game Over // 0 = moveSnake
-*/
-uint8_t checkCollisions(Point newPos);
 
-/*  Cuando el pulsador envia su dirección correspondiente, chequea si es valido
-    y si es así actualiza la dirección actual de la vibora
-*/
-void updateMovimiento(Direction new, Direction avoid);
+uint8_t checkCollisions(Point newPos);
+void updateDirection(Direction new, Direction avoid);
 
 /*  Genera posición random para nueva manzana
     - Chequea si la posición está ocupada por la vibora
     - Update de la posición al elemento "apple"
 */
 void newApple(); 
-
-// Genera la nueva posición de la vibora y si es válida la actualiza en el arreglo snake
 void moveSnake();
 
 //Chequea y envía los leds a encender a la matriz via I2P
@@ -58,9 +44,10 @@ void setDifficulty(uint16_t value);
 
 //Se encarga de enviar las estadisticas de la partida a la PC
 void sendStats();
-
-//Inicializa la vibora con tres leds de largo y su dirección + una manzana inicial
 void initGame();
+
+//Detiene el juego
+void stopGame();
 
 
 int main() {
@@ -72,12 +59,12 @@ int main() {
     return 0;
 }
 
+//Inicializa la vibora con tres leds de largo y su dirección + una manzana inicial
 void initGame(){
     // Inicializa la longitud de la vibora y su dirección
     snakeLength = 3;
     direction = DER;
 
-    // Inicializa la posición de la vibora
     snake[0].x = 0; snake[0].y = 4;
     snake[1].x = 0; snake[1].y = 5;
     snake[2].x = 0; snake[2].y = 6;
@@ -86,7 +73,8 @@ void initGame(){
     apple.y=6;
 }
 
-void moveSnake(){   //INCOMPLETO
+// Genera la nueva posición de la vibora y si es válida la actualiza en el arreglo snake
+void moveSnake(){   
     Point newPos = snake[0];
     if(direction==ARRIBA){
         newPos.y++;
@@ -98,13 +86,28 @@ void moveSnake(){   //INCOMPLETO
         newPos.x--;
     }
     if(!checkCollisions(newPos)){ //Es un movimiento válido
-        //Implementar update al arreglo de posiciones de la vibora
+        for(int i=snakeLength-1;i>0;i--){ //Muevo los puntos de la vibora un lugar a la derecha dentro del array
+            snake[i]=snake[i-1];
+        }
+        snake[0]=newPos; //Guardo la nueva posición de la cabeza de la vibora en la posición 0
+    } else{
+        stopGame();
+        //Sonido de GameOver
+        sendStats();
     }
 }
 
+/*  Chequea si el proximo movimiento newPos de la vibora contra cuatro situaciones:
+    - Fuera de los limites -> GameOver: Sonido + StopTotal + Enviar stats
+    - Choque contra si misma -> GameOver
+    - Choque con la manzana -> moveSnake + WIN: Sonido + newApple + updateLength
+    - Espacio libre -> moveSnake
+    ---
+       Return: -1 = Game Over // 0 = moveSnake
+*/
 uint8_t checkCollisions(Point newPos){
     //Caso 1: Fuera de los limites
-    if(newPos.x>=8 || newPos.x<0 || newPos.y>=8 || newPos.y<0){
+    if(newPos.x>=ANCHO || newPos.x<0 || newPos.y>=ALTO || newPos.y<0){
         return -1;
     }
     //Caso 2: Choque consigo misma
@@ -117,10 +120,19 @@ uint8_t checkCollisions(Point newPos){
     if (newPos.x==apple.x && newPos.y==apple.y){
         snakeLength++;
         newApple();
+        //Sonido de WIN
         return 0;
     }
     //Caso 4: Movimiento válido a espacio vacío
     return 0;
 }
 
+/*  Cuando el pulsador envia su dirección correspondiente, chequea si es valido
+    y si es así actualiza la dirección actual de la vibora
+*/
+void updateDirection(Direction new, Direction avoid){
+    if(direction!= new && direction!=avoid){
+        direction=new;
+    }
+}
 
